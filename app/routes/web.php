ac3c5207c4e150879c1d41cfd9be286d4c268111
Sergey Morozov <sergmoro1@ya.ru>
2022-05-31 15:49:33 +0300

    Добавил routes для PostController.

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post/create', [PostController::class, 'create']);
+Route::post('/post', [PostController::class, 'store']);
